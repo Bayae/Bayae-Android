bf5d9bfabed2246ef1be898dffc8a75be520b240
Rose <17xluo1@stu.edu.cn>
2019-11-24 21:53:00 +0800

    feat: user_info

---
+++ app/src/main/java/com/dudes/dexin/bayae/User_Info.java
@@ -0,0 +1,13 @@
+package com.dudes.dexin.bayae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class User_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user__info);
+    }
+}
