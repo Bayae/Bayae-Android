b1a01a3cda3431533b4fba900803470a23f64649
GeneLin <17jylin7@stu.edu.cn>
2019-12-08 22:39:57 +0800

    feat(model_select): the page of home page

--- app/src/main/java/com/dudes/dexin/bayae/study/ModelSelect.java
+++ app/src/main/java/com/dudes/dexin/bayae/study/ModelSelect.java
@@ -7,10 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.dudes.dexin.bayae.ModelInList;
 import com.dudes.dexin.bayae.R;
 
 
@@ -28,15 +30,23 @@ public class ModelSelect extends Activity {
         ListView list = (ListView)findViewById(R.id.list_select);
 
         ModelAdapter adapter = new ModelAdapter(this);
-        adapter.add(new Model("背书模式", "直接看答案",R.drawable.model_recite));
-        adapter.add(new Model("选择模式", "选择题",R.drawable.model_multiple_choice));
-        adapter.add(new Model("填空模式", "填空题",R.drawable.model_fill_blank));
-        adapter.add(new Model("混合模式", "二项全能",R.drawable.model_mix));
+        adapter.add(new ModelInList("背书模式", "直接看答案",R.drawable.model_recite));
+        adapter.add(new ModelInList("选择模式", "选择题",R.drawable.model_multiple_choice));
+        adapter.add(new ModelInList("填空模式", "填空题",R.drawable.model_fill_blank));
+        adapter.add(new ModelInList("混合模式", "二项全能",R.drawable.model_mix));
 
         list.setAdapter(adapter);
+
+        ImageButton btn = (ImageButton) findViewById(R.id.ib_title_back);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                ModelSelect.this.finish();
+            }
+        });
     }
 
-    class ModelAdapter extends ArrayAdapter<com.dudes.dexin.bayae.study.ModelSelect.Model> {
+    class ModelAdapter extends ArrayAdapter<ModelInList> {
         LayoutInflater inflator;
 
         public ModelAdapter(Context context) {
@@ -58,7 +68,7 @@ public class ModelSelect extends Activity {
                     .findViewById(R.id.list_subItem);
 
 
-            com.dudes.dexin.bayae.study.ModelSelect.Model s = this.getItem(position);
+            ModelInList s = this.getItem(position);
 
             img.setImageDrawable(getResources().getDrawable(s.image));
             item.setText(s.name);
@@ -67,15 +77,4 @@ public class ModelSelect extends Activity {
         }
     }
 
-    class Model {
-        String name;
-        String description;
-        int image;
-
-        public Model(String name, String description, int image) {
-            this.name = name;
-            this.description = description;
-            this.image = image;
-        }
-    }
 }
