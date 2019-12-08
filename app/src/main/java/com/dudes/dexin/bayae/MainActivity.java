c1334ebe8721688ab2476b833cbe192c39a1d222
GeneLin <17jylin7@stu.edu.cn>
2019-12-08 20:49:09 +0800

    feat(model_select): the page of selecting a study model

--- app/src/main/java/com/dudes/dexin/bayae/MainActivity.java
+++ app/src/main/java/com/dudes/dexin/bayae/MainActivity.java
@@ -1,14 +1,12 @@
 package com.dudes.dexin.bayae;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Button;
+
+import com.dudes.dexin.bayae.study.ModelSelect;
 
 public class MainActivity extends Activity {
 
@@ -17,5 +15,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn_modelSelect);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent it = new Intent();
+                it.setClass(MainActivity.this, ModelSelect.class);
+                MainActivity.this.startActivity(it);
+            }
+        });
     }
 }
--- app/src/main/java/com/dudes/dexin/bayae/study/ModelSelect.java
+++ app/src/main/java/com/dudes/dexin/bayae/study/ModelSelect.java
@@ -7,7 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+
 import com.dudes.dexin.bayae.R;
 
 
@@ -17,22 +20,20 @@ public class ModelSelect extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//
-//        ListView list = (ListView)findViewById(R.id.list);
-//        if(list==null)
-//        {
-//            android.util.Log.d("debug","list null");
-//
-//        }
-//
-//        ModelAdapter adapter = new ModelAdapter(this);
-//        adapter.add(new Model("背书模式", "直接看答案"));
-//        adapter.add(new Model("选择模式", "选择题"));
-//        adapter.add(new Model("填空模式", "填空题"));
-//        adapter.add(new Model("混合模式", "二项全能"));
-//
-//        list.setAdapter(adapter);
+        setContentView(R.layout.model_select);
+
+        TextView title = (TextView)findViewById(R.id.tv_title);
+        title.setText("模式选择");
+
+        ListView list = (ListView)findViewById(R.id.list_select);
+
+        ModelAdapter adapter = new ModelAdapter(this);
+        adapter.add(new Model("背书模式", "直接看答案",R.drawable.model_recite));
+        adapter.add(new Model("选择模式", "选择题",R.drawable.model_multiple_choice));
+        adapter.add(new Model("填空模式", "填空题",R.drawable.model_fill_blank));
+        adapter.add(new Model("混合模式", "二项全能",R.drawable.model_mix));
+
+        list.setAdapter(adapter);
     }
 
     class ModelAdapter extends ArrayAdapter<com.dudes.dexin.bayae.study.ModelSelect.Model> {
@@ -43,34 +44,38 @@ public class ModelSelect extends Activity {
             inflator = LayoutInflater.from(context);
         }
 
-//        @Override
-//        public View getView(int position, View convertView, ViewGroup parent) {
-//            if (convertView == null) {
-//                convertView = inflator.inflate(
-//                        android.R.layout.simple_list_item_2, parent, false);
-//            }
-//
-//            TextView text1 = (TextView) convertView
-//                    .findViewById(android.R.id.text1);
-//            TextView text2 = (TextView) convertView
-//                    .findViewById(android.R.id.text2);
-//
-//            com.dudes.dexin.bayae.MainActivity.Model s = this.getItem(position);
-//
-//            text1.setText(s.name);
-//            text2.setText("" + s.description);
-//
-//            return convertView;
-//        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = inflator.inflate(
+                        R.layout.model_item, parent, false);
+            }
+            ImageView img = (ImageView) convertView
+                    .findViewById(R.id.img);
+            TextView item = convertView
+                    .findViewById(R.id.list_item);
+            TextView subItem = convertView
+                    .findViewById(R.id.list_subItem);
+
+
+            com.dudes.dexin.bayae.study.ModelSelect.Model s = this.getItem(position);
+
+            img.setImageDrawable(getResources().getDrawable(s.image));
+            item.setText(s.name);
+            subItem.setText(s.description);
+            return convertView;
+        }
     }
 
     class Model {
         String name;
         String description;
+        int image;
 
-        public Model(String name, String description) {
+        public Model(String name, String description, int image) {
             this.name = name;
             this.description = description;
+            this.image = image;
         }
     }
 }
